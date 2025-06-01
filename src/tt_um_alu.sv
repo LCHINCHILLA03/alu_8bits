fdc15dfc9154fb9abc37dd96cc3f9c96518c7a24
LCHINCHILLA03 <lchinchillam3@miumg.edu.gt>
2025-06-01 13:26:37 -0600

    Update tt_um_alu.sv

--- src/tt_um_alu.sv
+++ src/tt_um_alu.sv
@@ -1,12 +1,19 @@
 `timescale 1ns / 1ps
 
 module tt_um_alu(
-               input logic clk,
-               input logic btnC, btnU,
                input logic [15:0] sw,
                output logic [7:0] led,
                output logic [3:0] an,
                output logic [6:0] seg
+               reg clk;
+               reg rst_n;
+               reg ena;
+               reg [7:0] ui_in;
+               reg [7:0] uio_in;
+               wire [7:0] uo_out;
+               wire [7:0] uio_out;
+               wire [7:0] uio_oe;
+  
                );
                
     logic clk_1;
@@ -15,7 +22,7 @@ module tt_um_alu(
     logic [7:0]b;
     logic [7:0] Y, ANDD, ORR;
     logic carry, cero, overflow;
-    Registros bits_entrada(.clk(clk_1), .rst(btnU), .btn_load(btnC), .sw1(sw[8:0]), .reg_a(a), .reg_b(b));
-    ALU ALU(.A(a), .B(b), .Shift(sw[15:12]), .S(sw[11:9]), .Y(led[7:0]), .ANDD(ANDD), .ORR(ORR), .Cero(cero), .Carry(carry), .Overflow(overflow));
-    flag_display flag(.clk(clk), .rst(btnU), .zero(cero), .carry(carry), .overflow(overflow),.an(an[3:0]), .seg(seg[6:0]));
+  Registros bits_entrada(.clk(clk_1), .rst(rst_n), .btn_load(ena), .sw1({uio_in[0],ui_in[7:0]}), .reg_a(a), .reg_b(b));
+  ALU ALU(.A(a), .B(b), .Shift(uio_in[4:1]), .S(uio_in[7:5]), .Y(uo_out[7:0]), .ANDD(ANDD), .ORR(ORR), .Cero(cero), .Carry(carry), .Overflow(overflow));
+  flag_display flag(.clk(clk), .rst(btnU), .zero(cero), .carry(carry), .overflow(overflow),.an(sio_oe[3:0]), .seg(uio_out[6:0]));
 endmodule
