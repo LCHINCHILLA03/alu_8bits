e590c68c29e46d258447a741aecd44ae94e2e4d9
LCHINCHILLA03 <lchinchillam3@miumg.edu.gt>
2025-06-01 15:56:46 -0600

    Update tt_um_alu.sv

--- src/tt_um_alu.sv
+++ src/tt_um_alu.sv
@@ -15,8 +15,10 @@ module tt_um_alu(
     clck_psc clck_psc(.clk(clk), .clk_scaled(clk_1));
     logic [7:0]a;
     logic [7:0]b;
+    logic [6:0]out1;
+    logic [3:0]out2;
     logic carry, cero, overflow;
     Registros bits_entrada(.clk(clk_1), .rst(rst_n), .btn_load(ena), .sw1({uio_in[0],ui_in[7:0]}), .reg_a(a), .reg_b(b));
   ALU ALU(.A(a), .B(b), .Shift(uio_in[4:1]), .S(uio_in[7:5]), .Y(uo_out[7:0]), .ANDD(uio_out[7:0]), .ORR(uio_oe[7:0]), .Cero(cero), .Carry(carry), .Overflow(overflow));
-  flag_display flag(.clk(clk), .rst(rst_n), .zero(cero), .carry(carry), .overflow(overflow),.an(a[3:0]), .seg(b[6:0]));
+  flag_display flag(.clk(clk), .rst(rst_n), .zero(cero), .carry(carry), .overflow(overflow),.an(out2[3:0]), .seg(out1[6:0]));
 endmodule
