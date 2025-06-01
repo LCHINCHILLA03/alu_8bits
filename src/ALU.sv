91bd93103222b835d944989f05fc4470f354dcef
LCHINCHILLA03 <lchinchillam3@miumg.edu.gt>
2025-06-01 14:29:40 -0600

    Update ALU.sv

--- src/ALU.sv
+++ src/ALU.sv
@@ -15,13 +15,13 @@ module ALU(
            logic [7:0]yASR;
            logic yzero;
            MUX2X1 MUX2(B, ~B, S0,ymux21);
-           carry_lookahead_adder Sumador1(A, ymux21, S0, ysum, Cout);
-           Shift_Left ShiftLeft(ysum, Shift, yASL);
-           Shift_Right ShiftRight(ysum, Shift, yASR);
+           carry_lookahead_adder Sumador1(.i_add1(A), .i_add2(ymux21), .carry(S0), .o_result(ysum), .cout(Cout));
+           Shift_Left ShiftLeft(.A(ysum), .Shift(Shift), .ASL(yASL));
+           Shift_Right ShiftRight(.A(ysum), .Shift(Shift), .ASL(yASR));
            assign ANDD = A & B;
            assign ORR = A | B;
-           Zero Zero1(ysum, yzero);
-           MUX6X1 MUX6(ysum, ysum ,ANDD, ORR, yASL, yASR, S, Y);
+           Zero Zero1(.A(ysum), .C(yzero));
+           MUX6X1 MUX6(.d0(ysum), .d1(ysum) ,.d2(ANDD), .d3(ORR), .d4(yASL), .d5(yASR), .s(S), .y(Y));
            assign Cero = yzero;
            assign Carry = ~S[0] & Cout;
            assign Overflow = (ysum[7] ^ A[7]) & ~(B[7] ^ A[7] ^ S0) & ~S[0] ;
